8334151e1d3fe3c5f01ce35d86d8fe58d6a0f81f
Jack Kleeman <jackkleeman@gmail.com>
2025-08-13 15:05:46 +0100

    Use u8 length prefix for encrypted dek

--- packages/journal-encryption-lib/src/stored_cipher_text.ts
+++ packages/journal-encryption-lib/src/stored_cipher_text.ts
@@ -3,7 +3,6 @@ import { webcrypto } from "node:crypto";
 export class StoredCipherText {
   private static STORED_PREFIX = new TextEncoder().encode("RTv1\0");
   public static IV_LENGTH = 12;
-  private static ENCRYPTED_KEY_LENGTH = 184;
 
   constructor(
     public encryptedDek: Uint8Array,
@@ -12,28 +11,36 @@ export class StoredCipherText {
   ) {}
 
   static fromBytes(data: Uint8Array): StoredCipherText {
-    if (
-      data.byteLength <=
-      this.STORED_PREFIX.length + this.ENCRYPTED_KEY_LENGTH + this.IV_LENGTH
-    ) {
-      throw new Error("Invalid data length");
-    }
-
     const prefix = new Uint8Array(data.slice(0, 0 + this.STORED_PREFIX.length));
 
-    if (!this.STORED_PREFIX.every((value, i) => value === prefix[i])) {
-      throw new Error("Invalid data prefix");
+    if (
+      prefix.length !== this.STORED_PREFIX.length ||
+      !this.STORED_PREFIX.every((value, i) => value === prefix[i])
+    ) {
+      throw new Error("Invalid stored ciphertext prefix");
     }
 
     const unprefixedData = data.slice(this.STORED_PREFIX.length);
 
-    const encryptedDek = unprefixedData.slice(0, this.ENCRYPTED_KEY_LENGTH);
+    if (unprefixedData.length == 0) {
+      throw new Error("Invalid stored ciphertext length");
+    }
+
+    const encryptedDekLength = unprefixedData[0];
+
+    const encryptedDek = unprefixedData.slice(1, encryptedDekLength + 1);
+    if (encryptedDek.length !== encryptedDekLength) {
+      throw new Error("Invalid stored ciphertext length");
+    }
     const iv = unprefixedData.slice(
-      this.ENCRYPTED_KEY_LENGTH,
-      this.ENCRYPTED_KEY_LENGTH + this.IV_LENGTH
+      encryptedDekLength + 1,
+      encryptedDekLength + 1 + this.IV_LENGTH
     );
+    if (iv.length !== this.IV_LENGTH) {
+      throw new Error("Invalid stored ciphertext length");
+    }
     const cipherText = unprefixedData.slice(
-      this.ENCRYPTED_KEY_LENGTH + this.IV_LENGTH
+      encryptedDekLength + 1 + this.IV_LENGTH
     );
 
     return new StoredCipherText(
@@ -70,30 +77,32 @@ export class StoredCipherText {
   }
 
   toBytes(): Uint8Array {
-    if (
-      this.encryptedDek.byteLength !== StoredCipherText.ENCRYPTED_KEY_LENGTH
-    ) {
-      throw new Error("Unexpected encrypted dek length");
+    if (this.encryptedDek.length > 255) {
+      throw new Error(
+        `StoredCipherText cannot store encrypted dek with a length of more than 255; dek was ${this.encryptedDek.length}`
+      );
     }
 
     const data = new Uint8Array(
       StoredCipherText.STORED_PREFIX.length +
-        StoredCipherText.ENCRYPTED_KEY_LENGTH +
+        1 +
+        this.encryptedDek.length +
         StoredCipherText.IV_LENGTH +
         this.cipherText.byteLength
     );
 
     data.set(StoredCipherText.STORED_PREFIX, 0);
-    data.set(this.encryptedDek, StoredCipherText.STORED_PREFIX.length);
+    data.set([this.encryptedDek.length], StoredCipherText.STORED_PREFIX.length);
+    data.set(this.encryptedDek, StoredCipherText.STORED_PREFIX.length + 1);
     data.set(
       this.iv,
-      StoredCipherText.STORED_PREFIX.length +
-        StoredCipherText.ENCRYPTED_KEY_LENGTH
+      StoredCipherText.STORED_PREFIX.length + 1 + this.encryptedDek.length
     );
     data.set(
       this.cipherText,
       StoredCipherText.STORED_PREFIX.length +
-        StoredCipherText.ENCRYPTED_KEY_LENGTH +
+        1 +
+        this.encryptedDek.length +
         StoredCipherText.IV_LENGTH
     );
 
