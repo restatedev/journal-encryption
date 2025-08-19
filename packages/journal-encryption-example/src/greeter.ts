f78c6886195c9ffd8431a48164acc47e6bb6f3c2
igalshilman <igal@restate.dev>
2025-08-19 10:54:39 +0200

    Add an example

---
+++ packages/journal-encryption-example/src/greeter.ts
@@ -0,0 +1,26 @@
+import { service, createServiceHandler, Context } from "@restatedev/restate-sdk";
+import { serde } from "@restatedev/restate-sdk-zod";
+import { z } from "zod";
+
+export const greeter = service({
+  name: "greeter",
+  handlers: {
+    greet: createServiceHandler(
+      {
+        input: serde.zod(
+          z.object({
+            name: z.string(),
+          })
+        ),
+        output: serde.zod(
+          z.object({
+            message: z.string(),
+          })
+        ),
+      },
+      async (context: Context, { name }) => {
+        return { message: `Hello, ${name}!` };
+      }
+    ),
+  },
+});
